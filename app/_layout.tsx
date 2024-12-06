50df315a5140adbf052458098894be992d6aac80
Axely7 <axely7@hotmail.com>
2024-12-06 15:28:55 -0600

    links

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -9,11 +9,10 @@ import { useEffect } from "react";
 import "react-native-reanimated";
 import { useColorScheme } from "@/hooks/useColorScheme";
 import "../global.css";
-import { Text, View } from "react-native";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { useThemeColor } from "../hooks/useThemeColor";
-import ThemedView from "@/presentation/shared/ThemedView";
-import ThemedText from "@/presentation/shared/ThemedText";
+import { Stack } from "expo-router";
+import { allRoutes } from "@/constants/Routes";
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
@@ -41,11 +40,33 @@ export default function RootLayout() {
       style={{ backgroundColor: backgrounColor, flex: 1 }}
     >
       <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
-        <ThemedView margin>
-          <ThemedText className="mt-20">Hola</ThemedText>
-        </ThemedView>
-
-        {/* <Stack></Stack> */}
+        <Stack
+          screenOptions={{
+            headerShadowVisible: false,
+            contentStyle: {
+              backgroundColor: backgrounColor,
+            },
+            headerStyle: {
+              backgroundColor: backgrounColor,
+            },
+          }}
+        >
+          <Stack.Screen
+            name="index"
+            options={{
+              title: "",
+            }}
+          />
+          {allRoutes.map((route) => (
+            <Stack.Screen
+              key={route.name}
+              name={route.name}
+              options={{
+                title: route.title,
+              }}
+            />
+          ))}
+        </Stack>
       </ThemeProvider>
     </GestureHandlerRootView>
   );
